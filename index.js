2371819ae83d8c544b5a6ed02beed6b2fc6eb1a7
samim1920 <sksamimali2021@gmail.com>
2025-07-01 23:44:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+console.log("chai 1920");
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('samimcom')
+
+})
+app.get('/login',(req,res) => {
+ res.send('<h1>samim</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
